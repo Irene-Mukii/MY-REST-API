4474efe0fcd18c43ed6aa52c9c98182bc13cbc77
renniequin@gmail.com <renniequin@gmail.com>
2022-10-04 13:26:36 +0300

    checks parsed request using inbuilt node module url

--- index.js
+++ index.js
@@ -5,10 +5,24 @@
 
 //dependancies
 const http = require('http');
+const url = require('url')
 
 //server should respond to all requests with a sting 
 const server = http.createServer(function(req,res){
+
+    //get url and parse it
+    const parsedUrl = url.parse(req.url, true); //true uses query string module that works with url module
+
+    //get path 
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\+|\/+$/g,'')  // trims Regular Expressions except in the middle of dtrings
+
+    //send response
     res.end('Hello World\n')
+
+    //log the request oath
+    console.log('Request received on path: '+ trimmedPath);
+    
 })
 
 //start the server, have it listen on port 3000 ? 
