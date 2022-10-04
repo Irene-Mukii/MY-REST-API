a139a96a0f91349996c0541996cc9dcc5870dbc4
renniequin@gmail.com <renniequin@gmail.com>
2022-10-04 13:56:44 +0300

    checks parsed http querystrings

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const url = require('url');
 const server = http.createServer(function(req,res){
 
     //get url and parse it
-    const parsedUrl = url.parse(req.url, true); //true uses query string module that works with url module
+    const parsedUrl = url.parse(req.url, true); //true (converts query string into key pair values) --from querystring inbuilt module
 
     //get path 
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\+|\/+$/g,'');  // trims Regular Expressions except in the middle of strings  (g searches globally the expressions instead if stopping at the first instance found)
 
+    //get query string as an object
+    let queryStringObject = parsedUrl.query;
 
     //get the http method
     const method = req.method.toLowerCase();
@@ -25,7 +27,7 @@ const server = http.createServer(function(req,res){
     res.end('Hello World\n');
 
     //log the request oath
-    console.log('Request received on path: '+ trimmedPath+ ' with this method: ' + method);
+    console.log('Request received on path: '+ trimmedPath+ ' with this method: ' + method + ' and with these querystring paramaters: ', queryStringObject);
     
 })
 
