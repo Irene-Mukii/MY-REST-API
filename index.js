942bb7ca439e60183ba5e3ed87e428a0ba605b8f
renniequin@gmail.com <renniequin@gmail.com>
2022-10-04 13:36:47 +0300

    checks parsed http methods using inbuilt node module http, previous one req.url was using http also

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 //dependancies
 const http = require('http');
-const url = require('url')
+const url = require('url');
 
 //server should respond to all requests with a sting 
 const server = http.createServer(function(req,res){
@@ -15,17 +15,21 @@ const server = http.createServer(function(req,res){
 
     //get path 
     const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\+|\/+$/g,'')  // trims Regular Expressions except in the middle of dtrings
+    const trimmedPath = path.replace(/^\+|\/+$/g,'');  // trims Regular Expressions except in the middle of strings  (g searches globally the expressions instead if stopping at the first instance found)
+
+
+    //get the http method
+    const method = req.method.toLowerCase();
 
     //send response
-    res.end('Hello World\n')
+    res.end('Hello World\n');
 
     //log the request oath
-    console.log('Request received on path: '+ trimmedPath);
+    console.log('Request received on path: '+ trimmedPath+ ' with this method: ' + method);
     
 })
 
 //start the server, have it listen on port 3000 ? 
 server.listen(3000,function(){
-    console.log('Server is listening on port 3000 now')
+    console.log('Server is listening on port 3000 now');
 })
